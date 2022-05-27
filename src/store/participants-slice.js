ec714d5f802206f0323cd45f4de6df9f8c013a52
tsuipz <tsuipz@gmail.com>
2022-05-27 16:18:22 -0400

    Updated code to include a filter for funds: will remove later after raffle event

--- src/store/participants-slice.js
+++ src/store/participants-slice.js
@@ -17,32 +17,38 @@ const participantsSlice = createSlice({
 			const csvHeader = action.payload.slice(0, action.payload.indexOf('\n')).split(',');
 			const csvRows = action.payload.slice(action.payload.indexOf('\n') + 1).split('\n');
 
-			const array = csvRows.map((i) => {
-				const titles = {
-					Amount: true,
-					Email: true,
-					'Employee name': true,
-				};
-				const values = i.split(',');
-				const obj = csvHeader.reduce((object, header, index) => {
-					// This is to handle end cases of \r when spliting csv files
-					const title = header.replace('\r', '');
-					if (title in titles) {
-						if (title === 'Amount') {
-							object[title] = parseInt(values[index].replace('$', ''));
-						} else {
-							object[title] = values[index];
+			const array = csvRows
+				.map((i) => {
+					// TODO: Email should be 'Employee email'
+					const titles = {
+						Amount: true,
+						'Employee email': true,
+						'Employee name': true,
+						Funds: true, // REMOVE LATER
+					};
+					const values = i.split(',');
+					const obj = csvHeader.reduce((object, header, index) => {
+						// This is to handle end cases of \r when spliting csv files
+						const title = header.replace('\r', '');
+						if (title in titles) {
+							if (title === 'Amount' && !!values[index]) {
+								object[title] = parseInt(values[index].replace('$', ''));
+							} else {
+								object[title] = values[index];
+							}
+							state.receivedUsers = true;
 						}
-						state.receivedUsers = true;
-					}
-					return object;
-				}, {});
-				return obj;
-			});
+						return object;
+					}, {});
+					return obj;
+				})
+				.filter((obj) => obj.Funds === 'AAPIH AFam Raffle Fund');
 
+			console.log(array);
 			// Creates tickets for participants
 			for (let idx = 0; idx < array.length; idx++) {
-				const { Amount: amount, Email: email, 'Employee name': name } = array[idx];
+				// TODO: Email should be 'Employee email'
+				const { Amount: amount, 'Employee email': email, 'Employee name': name } = array[idx];
 				let length = Math.floor(amount / 5);
 				while (length > 0) {
 					maxParticipants.push({ name, email });
@@ -51,13 +57,15 @@ const participantsSlice = createSlice({
 			}
 
 			// Shuffles the array to be random
-			for (var i = maxParticipants.length - 1; i > 0; i--) {
+			for (let i = maxParticipants.length - 1; i > 0; i--) {
 				// Grabs a random index and swaps with the current idx;
-				var j = Math.floor(Math.random() * (i + 1));
-				var temp = maxParticipants[i];
+				const j = Math.floor(Math.random() * (i + 1));
+				const temp = maxParticipants[i];
 				maxParticipants[i] = maxParticipants[j];
 				maxParticipants[j] = temp;
 			}
+
+			console.log(maxParticipants);
 
 			state.users = maxParticipants;
 		},
